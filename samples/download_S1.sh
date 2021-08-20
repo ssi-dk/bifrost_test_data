29d5ff388e0cf2b060c04fa13f44d491b005d29a
Kristoffer Kiil <krki@ssi.dk>
2021-08-20 14:02:06 +0200

    Fixed output files in sample download script

--- samples/download_S1.sh
+++ samples/download_S1.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 [ ! -e "S1_R1.fastq.gz" ] && wget -q -O S1_R1.fastq.gz "ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR430/ERR4301030/S1.R1.fastq.gz"
 [ ! -e "S1_R2.fastq.gz" ] && wget -q -O S1_R2.fastq.gz "ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR430/ERR4301030/S1.R2.fastq.gz"
-[ ! -e "SRR2094561_1.fastq.gz" ] && wget -q -O S1_R1.fastq.gz "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/001/SRR2094561/SRR2094561_1.fastq.gz"
-[ ! -e "SRR2094561_2.fastq.gz" ] && wget -q -O S1_R2.fastq.gz "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/001/SRR2094561/SRR2094561_2.fastq.gz"
+[ ! -e "SRR2094561_1.fastq.gz" ] && wget -q -O SRR2094561_1.fastq.gz "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/001/SRR2094561/SRR2094561_1.fastq.gz"
+[ ! -e "SRR2094561_2.fastq.gz" ] && wget -q -O SRR2094561_2.fastq.gz "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/001/SRR2094561/SRR2094561_2.fastq.gz"
