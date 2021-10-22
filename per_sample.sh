122aabc3eeb72f8d5cbcb55acfd87fddf98d4c22
githubz0r <lc@MacBook-Pro-4.local>
2021-10-22 14:40:40 +0200

    updated variable for component subset

--- per_sample.sh
+++ per_sample.sh
@@ -5,7 +5,7 @@ mkdir $sample.name;
 cd $sample.name;
 
 # for IMAGE in bifrost_min_read_check
-components=$run.componentsubset # comma separated string
+components=$run.component_subset # comma separated string
 for IMAGE in $(echo $components | tr "," " ") # replacing commas with spaces for loop
 do
     docker run -w $BIFROST_DIR/output/$sample.name --env BIFROST_DB_KEY --mount type=bind,source=$BIFROST_DIR,target=$BIFROST_DIR ssidk/$IMAGE:latest -name $sample.name
